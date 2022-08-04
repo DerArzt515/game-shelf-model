91e2d5da92e6823c82c07ab6f4d67fb56a109a4a
Matt Cavalier <matthewjcavalier@pm.me>
2022-08-04 15:32:46 -0500

    checkin

--- src/commonMain/kotlin/Game.kt
+++ src/commonMain/kotlin/Game.kt
@@ -1,13 +1,34 @@
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class Game(
-    val id: Int?,
+data class GameCreationRequest(
     val title: String,
     val playerCount: MinMax,
     val playTime: MinMax,
     val publishers: List<String>,
     val designers: List<String>
+)
+
+@Serializable
+data class Game(
+    val id: Int,
+    val title: String,
+    val playerCount: MinMax,
+    val playTime: MinMax,
+    val publishers: List<Publisher>,
+    val designers: List<Designer>
+)
+
+@Serializable
+data class Publisher(
+    val id: Int,
+    val name: String
+)
+
+@Serializable
+data class Designer(
+    val id: Int,
+    val name: String
 )
 
 @Serializable
